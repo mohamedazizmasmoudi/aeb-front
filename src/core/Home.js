8317bf032f2994675c4c36b20a2dfb98063d33da
mohamedazizmasmoudi <55623593+mohamedazizmasmoudi@users.noreply.github.com>
2020-03-27 17:45:53 +0100

    lets start

---
+++ src/core/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Posts from "../post/Posts";
+
+const Home = () => ( <
+    div >
+    <
+    div className = "jumbotron" >
+    <
+    h2 > Home < /h2> <
+    p className = "lead" > Welcome to React Frontend < /p> <
+    /div> <
+    div className = "container" >
+    <
+    Posts / >
+    <
+    /div> <
+    div > hi deglet < /div> <
+    /div>
+);
+
+export default Home;
