9d0f57a959c9918bdd942dd9e4349742c541c1f3
mohamedazizmasmoudi <55623593+mohamedazizmasmoudi@users.noreply.github.com>
2020-03-29 17:56:23 +0100

    navbar

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,6 +1,20 @@
 import React, { Fragment } from 'react';
+import "./Menu.css";
 import { Link, withRouter } from 'react-router-dom';
 import { signout, isAuthenticated } from '../auth';
+import{Navbar,NavDropdown,Form,FormControl,Nav,Button}from'react-bootstrap';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menuu from '@material-ui/core/Menu';
 
 const isActive = (history, path) => {
     if (history.location.pathname === path) return { color: '#ff9900' };
@@ -8,84 +22,126 @@ const isActive = (history, path) => {
 };
 
 const Menu = ({ history }) => (
-    <div>
-        <ul className="nav nav-tabs bg-primary">
-            <li className="nav-item">
-                <Link className="nav-link" style={isActive(history, '/')} to="/">
-                    Home
-                </Link>
-            </li>
-
-            <li className="nav-item">
-                <Link
-                    className={history.location.pathname === '/users' ? 'active nav-link' : 'not-active nav-link'}
-                    to="/users"
-                >
-                    Users
-                </Link>
-            </li>
-
-            <li className="nav-item">
-                <Link to={`/post/create`} style={isActive(history, `/post/create`)} className="nav-link">
-                    Create Post
-                </Link>
-            </li>
-
-            {!isAuthenticated() && (
-                <React.Fragment>
-                    <li className="nav-item">
-                        <Link className="nav-link" style={isActive(history, '/signin')} to="/signin">
-                            Sign In
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">
-                            Sign Up
-                        </Link>
-                    </li>
-                </React.Fragment>
-            )}
-
+    <div >    
+{/* 
             {isAuthenticated() && isAuthenticated().user.role === 'admin' && (
                 <li className="nav-item">
                     <Link to={`/admin`} style={isActive(history, `/admin`)} className="nav-link">
                         Admin
                     </Link>
                 </li>
-            )}
+            )} */}<AppBar position="static">
 
-            {isAuthenticated() && (
-                <React.Fragment>
-                    <li className="nav-item">
-                        <Link to={`/findpeople`} style={isActive(history, `/findpeople`)} className="nav-link">
-                            Find People
-                        </Link>
-                    </li>
+        <Navbar expand="lg"className="bo">
+        <Typography variant="h6" className="title">
+  <Link className="nav-link" style={isActive(history, '/')} to="/">
+                    Home
+                </Link>
+    </Typography>
+                {/* <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form> */}
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Toolbar>
+  <Navbar.Collapse id="basic-navbar-nav">
+  <Typography variant="h6" className="title">
+  <Link className="nav-link" style={isActive(history, '/')} to="/">
+                    
+                </Link>
+    </Typography>
 
-                    <li className="nav-item">
-                        <Link
+
+
+    {isAuthenticated() && (
+
+<Link
+                  className={history.location.pathname === '/users' ? 'active nav-link' : 'not-active nav-link'}
+                  to="/users"
+              >
+<IconButton edge="start" className="menuButton" color="inherit" aria-label="menu">
+    <MenuIcon />
+  </IconButton>                </Link> 
+  )}
+    {isAuthenticated() && (
+
+  <Link to={`/post/create`}  className="nav-link">
+<IconButton edge="start" className="menuButton" color="inherit" aria-label="menu">
+    <MenuIcon />
+  </IconButton>                </Link>
+    )}
+        {!isAuthenticated() && (
+
+                      <Link className="nav-link" style={isActive(history, '/signin')} to="/signin">
+                      <IconButton edge="start" className="menuButton" color="inherit" aria-label="menu">
+    <MenuIcon />
+  </IconButton>                              </Link>
+   
+    )}
+{!isAuthenticated() && (
+
+<Link className="nav-link" style={isActive(history, '/')} to="/signin">
+<IconButton edge="start" className="menuButton" color="inherit" aria-label="menu">
+<MenuIcon />
+</IconButton>                              </Link>
+
+)}{isAuthenticated() && (
+
+  <Link
                             to={`/user/${isAuthenticated().user._id}`}
-                            style={isActive(history, `/user/${isAuthenticated().user._id}`)}
-                            className="nav-link"
-                        >
+                            
+                            className="nav-linkk"
+                        > <IconButton
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          color="inherit"
+        >           
+          <AccountCircle />
+        </IconButton>
                             {`${isAuthenticated().user.name}'s profile`}
-                        </Link>
-                    </li>
+                        </Link> )}
+       
+        
 
-                    <li className="nav-item">
-                        <span
-                            className="nav-link"
-                            style={{ cursor: 'pointer', color: '#fff' }}
-                            onClick={() => signout(() =>{ history.push('/')
-                            window.location.reload(false)
-                        })}
-                        >
-                            Sign Out
-                        </span>
-                    </li>
-                </React.Fragment>
-            )}
-        </ul>
+
+        {isAuthenticated() && (
+
+<NavDropdown title="Dropdown" id="basic-nav-dropdown" className="nav-link">
+
+
+<NavDropdown.Item href="/findpeople"style={isActive(history, `/findpeople`)}> 
+<Link to={`/findpeople`}   className="nav-linkk" style={{    color:'black'
+            }}> <MenuItem >
+            Find People</MenuItem>
+        </Link></NavDropdown.Item>
+<NavDropdown.Divider />
+
+<NavDropdown.Item >        <span style={{ cursor: 'pointer', color: '#fff' }}
+            onClick={() => signout(() => {
+                history.push('/')
+                window.location.reload(true)
+            })}
+            className="nav-linkk" style={{    color:'black'
+            }}
+           
+        >
+            Sign Out
+        </span></NavDropdown.Item>
+</NavDropdown>
+)}
+
+
+    
+  
+
+  </Navbar.Collapse></Toolbar>
+</Navbar></AppBar>
+
+<FormGroup>
+
+</FormGroup>
+
     </div>
 );
 
