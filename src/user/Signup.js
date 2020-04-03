c424f6ee508ac003f1cfc06c928cd78ffea7c40c
mohamedazizmasmoudi <55623593+mohamedazizmasmoudi@users.noreply.github.com>
2020-04-03 15:54:44 +0100

    go

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import { signup } from "../auth";
+import {  Redirect } from "react-router-dom";
+import { signin, authenticate } from "../auth";
 import SocialLogin from "./SocialLogin";
-import "./signup.css";
+import "./sigin.css";
 
-  import Avatar from '@material-ui/core/Avatar';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -20,15 +21,15 @@ import Container from '@material-ui/core/Container';
 
 
 
-class Signup extends Component {
+class Signin extends Component {
     constructor() {
         super();
         this.state = {
-            name: "",
             email: "",
             password: "",
             error: "",
-            open: false,
+            redirectToReferer: false,
+            loading: false,
             recaptcha: true
         };
     }
@@ -38,141 +39,157 @@ class Signup extends Component {
         this.setState({ [name]: event.target.value });
     };
 
-;
+
 
     clickSubmit = event => {
         event.preventDefault();
-        const { name, email, password } = this.state;
+        this.setState({ loading: true });
+        const { email, password } = this.state;
         const user = {
-            name,
             email,
             password
         };
         // console.log(user);
         if (this.state.recaptcha) {
-            signup(user).then(data => {
-                if (data.error) this.setState({ error: data.error });
-                else
-                    this.setState({
-                        error: "",
-                        name: "",
-                        email: "",
-                        password: "",
-                        open: true
+            signin(user).then(data => {
+                if (data.error) {
+                    this.setState({ error: data.error, loading: false });
+                } else {
+                    // authenticate
+                    authenticate(data, () => {
+                        this.setState({ redirectToReferer: true });
                     });
+                }
             });
         } else {
             this.setState({
+                loading: false,
                 error: "What day is today? Please write a correct answer!"
             });
         }
     };
 
-
     render() {
-        const { name, email, password, error, open, recaptcha } = this.state;
-        return (
-            
-             <Container component="main" maxWidth="xs">
-             <CssBaseline />
-             <div className="paper">
-               <Avatar className="avatar" style={{    backgroundColor: 'blue'}}>
-                 <LockOutlinedIcon />
-               </Avatar>
-               <Typography component="h1" variant="h5">
-                 Sign up
-               </Typography>
-               <form className="form" noValidate>
-                 <Grid container spacing={2}>
-                   <Grid item xs={12} >
-                     <TextField
-                       onChange={this.handleChange("name")}
-                    type="text"
-                    value={name}
-                      autoComplete="name"
-                       name="Name"
-                       variant="outlined"
-                       required
-                       fullWidth
-                       id="Name"
-                       label="Name"
-                       fullWidth
 
-                       autoFocus
-                     />
-                   </Grid>
-             
-                   <Grid item xs={12}>
-                     <TextField
-                          onChange={this.handleChange("email")}
-                          type="email"
-                          value={email}
-                       variant="outlined"
-                       required
-                       fullWidth
-                       id="email"
-                       label="Email Address"
-                       name="email"
-                       autoComplete="email"
-                     />
-                   </Grid>
-                   <Grid item xs={12}>
-                     <TextField
-                            onChange={this.handleChange("password")}
-                            type="password"
-                            value={password}
-                       variant="outlined"
-                       required
-                       fullWidth
-                       name="password"
-                       label="Password"
-                       type="password"
-                       id="password"
-                       autoComplete="current-password"
-                     />
-                   </Grid>
-                   <Grid item xs={12}>
-                     <FormControlLabel
-                       control={<Checkbox value="allowExtraEmails" color="primary" />}
-                       label="I want to receive inspiration, marketing promotions and updates via email."
-                     />
-                   </Grid>
-                 </Grid>
-                 <Button
-                                 onClick={this.clickSubmit}
-                                 style={{marginTop: 24 + 'px', marginBottom: 24 + 'px' }}
-                   type="submit"
-                   fullWidth
-                   variant="contained"
-                   color="primary"
-                   className="submit"
-                 >
-                   Sign Up
-                 </Button>
-                 <div
+        const {
+            email,
+            password,
+            error,
+            redirectToReferer,
+            loading,
+            recaptcha
+        } = this.state;
+
+        if (redirectToReferer) {
+            return <Redirect to="/" />;
+        }
+  
+        const classes =  makeStyles((theme) => ({
+          paper: {
+            marginTop: theme.spacing(8),
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          },
+          avatar: {
+            margin: theme.spacing(1),
+            backgroundColor: theme.palette.secondary.main,
+          },
+          form: {
+            width: '100%', // Fix IE 11 issue.
+            marginTop: theme.spacing(3),
+          },
+          submit: {
+            margin: theme.spacing(3, 0, 2),
+          },
+        }));
+        return (
+
+
+          <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <div className="paper">
+            <Avatar className="avatar" style={{    backgroundColor: 'blue'}}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <form className="form" noValidate>
+              <TextField
+               onChange={this.handleChange("email")}
+               type="email"
+               value={email}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                  onChange={this.handleChange("password")}
+                  type="password"
+                  value={password}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                              onClick={this.clickSubmit}
+
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className="submit jss4"
+                style={{marginTop: 24 + 'px', marginBottom: 24 + 'px' }}
+              >
+                Sign In
+              </Button>
+              
+              <div
                     className="alert alert-danger"
                     style={{ display: error ? "" : "none" }}
                 >
                     {error}
                 </div>
 
-                <div
-                    className="alert alert-info"
-                    style={{ display: open ? "" : "none" }}
-                >
-                    New account is successfully created. Please{" "}
-                    <Link to="/signin">Sign In</Link>.
-                </div>
-                 <Grid container justify="flex-end">
-                   <Grid item>
-                     <Link href="/" variant="body2" className="opa">
-                       Already have an account? Sign in
-                     </Link>
-                   </Grid>
-                 </Grid>
-               </form>
-             </div>
-            <Box mt={5}>
+                {loading ? (
+                    <div className="jumbotron text-center">
+                        <h2>Loading...</h2>
+                    </div>
+                ) : (
+                    ""
+                )}
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2" className="opa">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="/signup" variant="body2" className="opa">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </form>
+          </div>
+          <Box mt={8}>
           <Typography variant="body2" color="textSecondary" align="center">
       {'Made with ❤️ by Aziz Masmoudi '}
       {/* <Link color="inherit" href="https://material-ui.com/">
@@ -181,9 +198,9 @@ class Signup extends Component {
       {new Date().getFullYear()}
       {'.'} */}
     </Typography>          </Box>
-           </Container>
+        </Container>         
         );
     }
 }
 
-export default Signup;
+export default Signin;
